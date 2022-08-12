fbf604eeba809e52b84a4d80415a25d28d86129e
mauriciomuniz <mauricio.santana2010@hotmail.com>
2022-08-12 04:21:05 -0300

    Update loginController.js
    fazendo login e logout

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,9 @@
 const Login = require ('../models/LoginModel')
 
 exports.index = (req,res) =>{
+    if(req.session.user) return res.render('logado');
     res.render('login');
+    
 };
 exports.register = async (req,res) =>{
 
@@ -30,3 +32,38 @@ exports.register = async (req,res) =>{
 
         }
     };
+
+exports.login = async (req,res) =>{
+
+        try{
+        const login = new Login(req.body);
+        await login.login();
+
+        console.log('passa')
+
+        if(login.errors.length > 0){
+            req.flash('errors',login.errors);
+            req.session.save(function(){
+                return res.redirect('../login/index');
+            });
+            return;
+        }
+
+
+        req.flash('success','Login realizado com sucesso');
+        req.session.user = login.user;
+        req.session.save(function(){
+            return res.redirect('../login/index');
+        });
+
+        }catch(e){
+            console.log(e);
+            return  res.render('deuRuim')
+
+        }
+    }; 
+
+exports.logout = function(req,res){
+    req.session.destroy();
+    res.redirect('/');
+}
