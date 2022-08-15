439f403dcd5d6ebe9213aec7b7f7e004bafc4708
mauriciomuniz <mauricio.santana2010@hotmail.com>
2022-08-15 19:41:00 -0300

    Update routes.js
    adicionando rota de login

--- routes.js
+++ routes.js
@@ -21,5 +21,5 @@ route.get('/login/logout', loginController.logout);
 //rota de contatos
 route.get('/contato/index',loginRequired, contatoController.index);
 route.post('/contato/register',loginRequired, contatoController.register);
-
+route.get('/contato/index/:id',loginRequired, contatoController.editIndex);
 module.exports = route;
