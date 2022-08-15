4da3340168292850f4cb7ef76abc4fb211669733
mauriciomuniz <mauricio.santana2010@hotmail.com>
2022-08-15 03:48:22 -0300

    Create contatoController.js
    criando o controler de contato.
    adicionando um contato na agenda

---
+++ src/controllers/contatoController.js
@@ -0,0 +1,26 @@
+const Contato = require('../models/ContatoModel');
+exports.index = (req,res) =>{
+    res.render('contato');
+};
+
+exports.register = async(req,res) =>{
+    try{
+        const contato = new Contato(req.body);
+        await contato.register();
+        
+        if(contato.errors.length > 0){
+            req.flash('errors', contato.errors);
+            req.session.save(()=>res.redirect('../contato/index'));
+            return;
+        }
+        req.flash('success', 'contato registrado com sucesso');
+        req.session.save(()=>res.redirect(`/contato/index/${contato.contato._id}`));
+        return;
+    }catch(e){
+        console.log(e);
+        return res.render('deuRuim');
+    }
+    
+    
+};
+
