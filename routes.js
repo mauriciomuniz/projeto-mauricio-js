cdb59dd1baa48018da08cecb8f7981c7b5d4425d
mauriciomuniz <mauricio.santana2010@hotmail.com>
2022-08-15 03:47:24 -0300

    Update routes.js
    criando as rotas de contato

--- routes.js
+++ routes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController')
 const loginController = require('./src/controllers/loginController')
+const contatoController = require('./src/controllers/contatoController')
+
+const {loginRequired} = require('./src/middlewares/middleware');
 //const pagina = require('./src/controllers/pagina')
 
 
@@ -9,11 +12,14 @@ const loginController = require('./src/controllers/loginController')
 route.get('/', homeController.index);
 route.post('/', homeController.recebeInicial);
 
-//rota de outra pagina
-//  route.get('/pagina', pagina.paginaInicial);
-
+//rota de login
 route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
+
+//rota de contatos
+route.get('/contato/index',loginRequired, contatoController.index);
+route.post('/contato/register',loginRequired, contatoController.register);
+
 module.exports = route;
