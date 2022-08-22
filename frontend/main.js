c13753c08868d7f954dda8471c571c26212a71c8
mauriciomuniz <mauricio.santana2010@hotmail.com>
2022-08-22 03:50:08 -0300

    correções

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,7 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 // import './assets/css/style.css';
+console.log("passa no main.js")
 import Login from './modules/Login';
 const login = new Login('.form-login');
 const register = new Login('.form-register');
